f60c893d3f65ea8eda35c978dc34472104bd6b82
kacper <kacpercz99@student.kul.lublin.pl>
2022-06-07 17:42:52 +0200

    init commit

--- src/Klawiatura.java
+++ src/Klawiatura.java
@@ -12,7 +12,7 @@ public class Klawiatura extends JPanel {
                 "7", "8", "9", "*",
                 "4", "5", "6", "-",
                 "1", "2", "3", "+",
-                "+/-", "0", ".", "="
+                "\u221A", "0", ".", "="
         };
 
         setLayout(new GridLayout(6, 4));
@@ -29,82 +29,95 @@ public class Klawiatura extends JPanel {
     }
 
     private record ActionListenerKlawiatury(String ktoryPrzycisk) implements ActionListener {
-            private static String symbol = "";
-            private static boolean errorFlag = false;
+        private static String symbol = "";
+        private static boolean errorFlag = false;
 
         @Override
-            public void actionPerformed(ActionEvent e) {
-                if (errorFlag) {
+        public void actionPerformed(ActionEvent e) {
+            if (errorFlag) {
+                Calculator.wartoscWyniku = "0";
+                errorFlag = false;
+            }
+
+            switch (ktoryPrzycisk) {
+                case "0" -> {
+                    if (!Calculator.wartoscWyniku.equals("0")) Calculator.wartoscWyniku += "0";
+                }
+                case "." -> {
+                    if (!Calculator.wartoscWyniku.contains(".")) Calculator.wartoscWyniku += ".";
+                }
+                case "C" -> {
                     Calculator.wartoscWyniku = "0";
-                    errorFlag = false;
+                    Calculator.poprzednik = 0.0d;
                 }
+                case "CE" -> Calculator.wartoscWyniku = "0";
 
-                switch (ktoryPrzycisk) {
-                    case "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {
-                        if (Calculator.wartoscWyniku.equals("0")) Calculator.wartoscWyniku = ktoryPrzycisk;
-                        else Calculator.wartoscWyniku += ktoryPrzycisk;
-                    }
-                    case "0" -> {
-                        if (!Calculator.wartoscWyniku.equals("0")) Calculator.wartoscWyniku += "0";
-                    }
-                    case "." -> {
-                        if (!Calculator.wartoscWyniku.contains(".")) Calculator.wartoscWyniku += ".";
-                    }
-                    case "C" -> {
+                case "\u2190" -> {
+                    if (Calculator.wartoscWyniku.length() == 1) {
                         Calculator.wartoscWyniku = "0";
-                        Calculator.poprzednik = 0.0d;
+                    } else if (Calculator.wartoscWyniku.length() > 1) {
+                        Calculator.wartoscWyniku = Calculator.wartoscWyniku.substring(0, Calculator.wartoscWyniku.length() - 1);
                     }
-                    case "CE" -> Calculator.wartoscWyniku = "0";
-
-                    case "\u2190" -> {
-                        if (Calculator.wartoscWyniku.length() == 1) {
-                            Calculator.wartoscWyniku = "0";
-                        } else if (Calculator.wartoscWyniku.length() > 1) {
-                            Calculator.wartoscWyniku = Calculator.wartoscWyniku.substring(0, Calculator.wartoscWyniku.length() - 1);
+                }
+                case "M+" ->
+                        Calculator.wartoscPamieci = String.valueOf((Double.parseDouble(Calculator.wartoscPamieci) + Double.parseDouble(Calculator.wartoscWyniku)));
+                case "M-" ->
+                        Calculator.wartoscPamieci = String.valueOf((Double.parseDouble(Calculator.wartoscPamieci) - Double.parseDouble(Calculator.wartoscWyniku)));
+                case "MC" -> Calculator.wartoscPamieci = "0";
+                case "\u221A" -> {
+                    if (Double.parseDouble(Calculator.wartoscWyniku) > 0.0d) {
+                        Calculator.wartoscWyniku = String.valueOf(Math.sqrt(Double.parseDouble(Calculator.wartoscWyniku)));
+                    } else {
+                        Calculator.wartoscWyniku = "BŁĄD - NaN";
+                        errorFlag = true;
+                    }
+                }
+                case "+", "-", "*", "/" -> {
+                    Calculator.poprzednik = Double.parseDouble(Calculator.wartoscWyniku);
+                    Calculator.wartoscWyniku = "0";
+                    symbol = ktoryPrzycisk;
+                }
+                case "%" -> {
+                    if (!symbol.equals("")) {
+                        if (symbol.equals("/") || symbol.equals("*")) {
+                            Calculator.wartoscWyniku = String.valueOf((Double.parseDouble(Calculator.wartoscWyniku) / 100));
+                        } else {
+                            Calculator.wartoscWyniku = String.valueOf((Calculator.poprzednik / 100) * Double.parseDouble(Calculator.wartoscWyniku));
                         }
                     }
-                    case "M+" -> Calculator.wartoscPamieci = String.valueOf((Double.parseDouble(Calculator.wartoscPamieci) + Double.parseDouble(Calculator.wartoscWyniku)));
-                    case "M-" -> Calculator.wartoscPamieci = String.valueOf((Double.parseDouble(Calculator.wartoscPamieci) - Double.parseDouble(Calculator.wartoscWyniku)));
-                    case "MC" -> Calculator.wartoscPamieci = "0";
-                    case "+", "-", "*", "/" -> {
-                        Calculator.poprzednik = Double.parseDouble(Calculator.wartoscWyniku);
-                        Calculator.wartoscWyniku = "0";
-                        symbol = ktoryPrzycisk;
-                    }
-                    case "%" -> {
-                        if (!symbol.equals("")) {
-                            if (symbol.equals("/") || symbol.equals("*")) {
-                                Calculator.wartoscWyniku = String.valueOf((Double.parseDouble(Calculator.wartoscWyniku) / 100));
+                }
+                case "=" -> {
+                    switch (symbol) {
+                        case "+" ->
+                                Calculator.wartoscWyniku = String.valueOf(Double.parseDouble(Calculator.wartoscWyniku) + Calculator.poprzednik);
+                        case "-" ->
+                                Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik - Double.parseDouble(Calculator.wartoscWyniku));
+                        case "*" ->
+                                Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik * Double.parseDouble(Calculator.wartoscWyniku));
+                        case "/" -> {
+                            if (Calculator.wartoscWyniku.matches("0\\.?0*")) {
+                                Calculator.wartoscWyniku = "BŁĄD - X/0";
+                                errorFlag = true;
                             } else {
-                                Calculator.wartoscWyniku = String.valueOf((Calculator.poprzednik / 100) * Double.parseDouble(Calculator.wartoscWyniku));
+                                Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik / Double.parseDouble(Calculator.wartoscWyniku));
                             }
                         }
                     }
-                    case "=" -> {
-                        switch (symbol) {
-                            case "+" -> Calculator.wartoscWyniku = String.valueOf(Double.parseDouble(Calculator.wartoscWyniku) + Calculator.poprzednik);
-                            case "-" -> Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik - Double.parseDouble(Calculator.wartoscWyniku));
-                            case "*" -> Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik * Double.parseDouble(Calculator.wartoscWyniku));
-                            case "/" -> {
-                                if (Calculator.wartoscWyniku.matches("0\\.?0*")) {
-                                    Calculator.wartoscWyniku = "BŁĄD - X/0";
-                                    errorFlag = true;
-                                } else {
-                                    Calculator.wartoscWyniku = String.valueOf(Calculator.poprzednik / Double.parseDouble(Calculator.wartoscWyniku));
-                                }
-                            }
-                        }
-                        symbol = "";
-                    }
+                    symbol = "";
                 }
-
-                Calculator.wynik.setText(Calculator.wartoscWyniku);
-                Calculator.pamiec.setText("Pamiec:" + Calculator.wartoscPamieci);
-
-                if (Calculator.wartoscPamieci.equals("0")) Calculator.pamiec.setVisible(false);
-                else if (!Calculator.pamiec.isVisible()) {
-                    Calculator.pamiec.setVisible(true);
+                default -> {
+                    if (Calculator.wartoscWyniku.equals("0")) Calculator.wartoscWyniku = ktoryPrzycisk;
+                    else Calculator.wartoscWyniku += ktoryPrzycisk;
                 }
             }
+
+            Calculator.wynik.setText(Calculator.wartoscWyniku);
+            Calculator.pamiec.setText("Pamiec:" + Calculator.wartoscPamieci);
+
+            if (Calculator.wartoscPamieci.equals("0")) Calculator.pamiec.setVisible(false);
+            else if (!Calculator.pamiec.isVisible()) {
+                Calculator.pamiec.setVisible(true);
+            }
         }
+    }
 }
